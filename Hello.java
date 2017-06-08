48090721e9f8ada6386449e517ffa06ccb634ec4
yoges23 <yogivpy23@gmail.com>
2017-06-08 17:49:37 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int[] n=new int[5];
+for(i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
