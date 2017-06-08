f48afbc0a670e86b13c2830b7f7337318cf1f9a2
yoges23 <yogivpy23@gmail.com>
2017-06-08 17:51:15 +0530

    Create Word.java

---
+++ Word.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Word
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+for(i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
